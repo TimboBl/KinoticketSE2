ea1c09123dd5147441901f7aafbece1ace9823d1
MalteHusung <fipsy555@hotmail.de>
2016-07-07 22:52:48 +0200

    Maximale Euroanzahl gefixt

--- src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
+++ src/de/uni_hamburg/informatik/swt/se2/kino/fachwerte/Geldbetrag.java
@@ -121,7 +121,7 @@ public final class Geldbetrag
      */
 
     private final static Pattern _pattern = Pattern
-        .compile("0*?([1-9][0-9]{0,8})?(,([0-9][0-9]?))?");
+        .compile("0*?([1-9][0-9]{0,6})?(,([0-9][0-9]?))?");
 
     public static Geldbetrag stringToGeldbetrag(String betrag)
     {
